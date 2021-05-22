02ef8bfdbd9cc427d5004c357cccfbbc91d85f52
Abyalew Ambaneh <77543474+AbyalewAmbaneh@users.noreply.github.com>
2021-05-22 17:20:46 -0500

    Add files via upload

---
+++ Spring & Hibernate Integration/exercise13_1/exercise13_1/src/main/java/cs544/exercise13_1/App.java
@@ -0,0 +1,15 @@
+package cs544.exercise13_1;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		
+		try (ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml")) {
+			ICustomerService customerService = context.getBean("customerService", ICustomerService.class);
+
+			customerService.addCustomer("Frank Brown", "fbrown@acme.com", "mainstreet 5", "Chicago", "60613");
+		}
+	}
+}
